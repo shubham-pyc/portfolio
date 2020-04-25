8f0a4b04b4d9a821b2b0d65302c8a751238d8f23
Shubham <shubham.h.gupta@impetus.co.in>
2020-04-25 14:57:21 +0530

    updated linkedin-scrapper

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,6 +5,7 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/home/impadmin/Desktop/Projects/Javascript/portfolio/.cache/dev-404-page.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/home/impadmin/Desktop/Projects/Javascript/portfolio/src/pages/index.js")))
 }
 
